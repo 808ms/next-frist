a6e9fe6395cfe93391036a7b102bc9c50d5a1cbf
808ms <hunter96816@gmail.com>
2022-09-05 16:00:41 -1000

    Commit #3

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,11 +30,12 @@ import { MdContactPage } from "react-icons/md";
 import React from "react";
 import { Tooltip, Typography } from "@mui/material";
 import Link from "next/link";
-import background from "./page/image/img-home.jpg"
+
 
 function Home() {
   return (
     <>
+      <div className="h-screen bg-gradient-to-r from-sky-500 to-indigo-500"></div>
       <Head>
         <title>Home | 808ms</title>
         <link rel="icon" href="/favicon.ico" />
@@ -51,7 +52,7 @@ function Home() {
         </Tooltip>
         <Tooltip title="LinkedIn">
           <a
-            href="https://www.linkedin.com/in/casey-choi-7b47b1248/"
+            href="https://www.linkedin.com/in/casey-choi/"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -59,7 +60,7 @@ function Home() {
           </a>
         </Tooltip>
         <Tooltip title="Contact">
-          <Link href="/contact">
+          <Link href="/cont act">
             <a>
               <MdContactPage
                 style={{ color: "white", position: "relative", top: "-2" }}
@@ -70,16 +71,16 @@ function Home() {
         </Tooltip>
       </div>
       <br />
-      <p>ello, I&apos;m</p>
+      <p>Ello, I&apos;m</p>
       <Typography variant="h1" className="text-4xl tracking-wide">
         Casey Choi
       </Typography>
       <p className="pt-3">
-        I&apos;m a 17 year old aspiring web developer from Hololulu, Hawaii.
-        I&apos;m currently learning many coding languages , such as CSS, JS,
-        typescript, Tailwind, and React js. This is my
-        first website project with typescript I&apos;m learning this to gain
-        experience in learning about computer science.
+        Hello, I am a current high school senior in Honolulu, Hawaii.
+        I&apos;m currently self-learning coding starting with CSS, JS,
+        typescript, Tailwind, and Python. This is my first website
+        made with react.js I&apos;m learning this to gain
+        experience in web development. 
       </p>
 
       <br />
